2021e8c9835b82431e4b9b8f6f5d7780820e39a7
MasonChance <mason.fryberger@gmail.com>
2020-04-06 13:32:36 -0700

    html scaffold, notes and working process outlin in sales.js

---
+++ sales.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// seperate each object with  ` ==== Object Name ======= ` 
+/* 
+1. Outline All major steps
+    A. All Objects/Object template
+      i. all related object properties
+      ii. all object related methods
+    B. Tasks to be Completed
+      i. Categorize by scope,
+        a.global
+        b.object specific
+        c.relates to sales.html
+        d. relates to index.html
+        c. relates to style.css/page layout/structure.
+      ii. Categorize individual tasks in terms of ManBearPig format or Outside-in structuring and workflow
+*/
