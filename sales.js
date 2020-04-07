b7ac47756c2f5ea6ec31cf29316ca166b8768694
MasonChance <mason.fryberger@gmail.com>
2020-04-06 20:38:13 -0700

    sketched out basic function for adding to guestsPerHour array

--- sales.js
+++ sales.js
@@ -62,34 +62,47 @@ var seattleStore = {
   minGuestCount : 23,
   maxGuestCount : 65,
   avgCookiesGuest : 6.3,
- 
+  
   hoursOpen : ['6am: ','7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: '],  // function to push time-datatypes into each index based on Open-time && Close-time???? ask about this option for later. for now, manual entry as strings will do. 
+
+  guestPerHour : [],
   dailyGuestCount : function(minGuestCount, maxGuestCount){
     return Math.random() * (this.maxGuestCount-this.minGuestCount) + this.minGuestCount;
   },
+
+  rNguestCount : function(guestPerHourLength){
+    var arrayLength = 0;
+
+    do{
+     this.guestPerHour.push(this.dailyGuestCount[arrayLength]);
+     arrayLength ++
+    } while (this.guestPerHour.length < this.hoursOpen.length);
+       
+  },
+    
   
-  rNguestCount : [],
   
   expectedCookies : [],
 
 };
 
-
+seattleStore.dailyGuestCount(seattleStore.minGuestCount, seattleStore.maxGuestCount);
+seattleStore.rNguestCount(seattleStore.guestPerHour.length);
 console.log('dailyGuestCount: ' + seattleStore.dailyGuestCount(this.minGuestCount,this.maxGuestCount));
 
 //expected output array with 13 indicies containing random values
 
  
 function updateCount(rNguestCountLength){
-  
+  // what if I use a do-while{}instead. I need dailyGuestCount to be run and updated at least once and I only need it updated for a very specific but dynamic conditon>>>>>
   for(var dailyIndex = 0; seattleStore.rNguestCount.length < seattleStore.hoursOpen.length; dailyIndex ++){
     var updateCount = seattleStore.dailyGuestCount;
     return  seattleStore.rNguestCount.push(updateCount[dailyIndex]);
   }
 }
-updateCount(seattleStore.rNguestCount.length);
 
-console.log('rNguestCount: ' + seattleStore.rNguestCount.length);
-console.log('rNgCount[]: ' + seattleStore.rNguestCount);
+
+// even though this variable uses the  ` this ` keyword, since it is called into the object scope as a propertyValue, the keyword now has context to operate on. 
+console.log('guestPerHour:' + seattleStore.guestPerHour);
 
    
