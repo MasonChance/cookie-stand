138e4a480cb9a530184d5d7f812cf83990c94f78
MasonChance <mason.fryberger@gmail.com>
2020-04-10 20:53:53 -0700

    began work on table header row. moved README.md out of extFolder

--- constructor.js
+++ constructor.js
@@ -2,13 +2,9 @@
 console.log('hello, I work');
 
 /* EXTRACT THE LITERALS INTO CONSTRUCTORS
-TODO: create the methods using prototype
-        1. remember this
-        2. remember that some functions rely on others and be sure to call them with (); before doing anything else with the method. 
-        3. be cognizant of which data is dynamic. 
+
 TODO: create stand alone function  to create table header give it a class and an id for styling
 TODO: create stand alone function to create table footergive it a class and an id for styling
-TODO:  create new function for generating hours open array based on open time and close time. Stretch goal. for now just get it working
 
 */
 function Store(locationName, minGuestCount, maxGuestCount, avgCookiesGuest){
@@ -21,10 +17,11 @@ function Store(locationName, minGuestCount, maxGuestCount, avgCookiesGuest){
   this.guestPerHour = this.perHourCount();
   this.cookiesPerHour = this.ordersPerHour();
   this.expectedCookies = this.dailyTotal();
+  this.renderToPage = this.renderToPage();
 
 }
 
-// Store.prototype.hoursOpen = function(){
+// TODO: Store.prototype.hoursOpen = function(){
 //   // not previously coded. outputs array of the hours based on open close time. 
 //   // displays with "am" and "pm" concatonation.
 //   // `if-else(){}` to check if before noon or after noon. 
@@ -63,49 +60,67 @@ Store.prototype.dailyTotal = function(){
   
 };
 
-// Store.prototype.renderToPage = function(){
-//   //Target Table id 'overview'
-//   var tRowTargetEl = document.getElementById('overview');
-//     // Create <tr> then give it an ID = this.locationName
-//   var newRowEl = document.createElement('tr');
-//   newRowEl.id = this.locationName;
-//   // Append to <table#overview> 
-//   tRowTargetEl.appendChild(newRowEl);
-//   //Target <tr#this.locationName>
-//   var tHeaderTargetEl = document.getElementById(this.locationName);
-//   //Create <th> (no # needed)
-//   var newThEl = document.createElement('th');
-//   //create content: location name is first cell. 
-//   var newThContent = this.locationName;
-//   newThEl.textContent = newThContent;
-//   tHeaderTargetEl.appendChild(newThEl);
-//     // next peice gets for(){}. this.hoursOpen.length
-    
-//     // for(var i = 0; i < this.hoursOpen.length; i++){
-//     //   var tDataTargetEl = document.getElementById(this.locationName);
-//     // // target <tr#this.locationName>
-//     // // Create Content
-//     // // new <td>
-//     // var newTdEl = document.createElement('td');
-//     // // newContent this.cookiesPerHour[i]
-//     // var newTdContent = this.cookiesPerHour[i];
-//     // // assign new content<td.textContent>
-//     // newTdEl.textContent = newTdContent;
-//     // //append to target tr#this.locationName.
-//     // tDataTargetEl.appendChild(newTdEl);
-//   // }
+//===== The Table Header is Independent of the Store Data Rendering Function ====//
+//FIXME:
+Store.prototype.renderStoreTableHeader = function(){
+  var headerTargetRow = document.getElementById('overview');
+  var newHeaderRowEl = document.createElement('tr');
+  newHeaderRowEl.id = 'operation-hours';
+  headerTargetRow.appendChild(newHeaderRowEl);
 
+  var operationThElTarget = document.getElementById('operation-hours');
+  var newThEl = document.createElement('th');
+  operationThElTarget.appendChild(newThEl);
+
+  for(var i = 0; i < this.hoursOpen.length; i++){
+    var hoursOpenThEl = document.createElement('th');
+    var newHrOpenContent = this.hoursOpen[i];
+    hoursOpenThEl.textContent(newHrOpenContent);
+    operationThElTarget.appendChild(newHrOpenContent);
+  }
+};
+
+Store.prototype.renderToPage = function(){
+
+  //segment creates new row with id and appends to the table with the locationName in a <th> that is the first child of the row
+  var tRowTargetEl = document.getElementById('overview');
+  var newRowEl = document.createElement('tr');
+  newRowEl.id = this.locationName;
+  newRowEl.textContent + this.locationName;
+  tRowTargetEl.appendChild(newRowEl);
   
-//   console.log('newThContent : ' + newThContent);
-//   console.log('newThEl.textContent: ' + newThEl.textContent);
-//   console.log('newRowElId: ' + newRowEl.id);
-// };
+  // creates new <td> elements at the row#this.location name. fills with content = this.cookiesPerHour[i]
+  var tHeaderTargetEl = document.getElementById(this.locationName);
+  var newThEl = document.createElement('th');
+  var newThContent = this.locationName;
+  newThEl.textContent = newThContent;
+  tHeaderTargetEl.appendChild(newThEl);
+    
+  for(var i = 0; i < this.hoursOpen.length; i++){
+    var tDataTargetEl = document.getElementById(this.locationName);
+    var newTdEl = document.createElement('td');
+    var newTdContent = this.cookiesPerHour[i];
+    
+    newTdEl.textContent = newTdContent;
+    tDataTargetEl.appendChild(newTdEl);
+    
+  }
+  
+};
 
 
-// var seattle = new Store('Seattle', 23, 65,6.3);
 
-// seattle.renderToPage();
-//========== consolelogs for Rendertopage =====
+
+//==== Create Store Objects Here ====//
+var seattle = new Store('Seattle', 23, 65,6.3);
+// var tokyo = new Store('Tokyo', 3, 24, 1.2);
+// var dubai = new Store('Dubai', 11, 38, 3.7);
+// var paris = new Store('Paris', 20, 38, 2.3);
+// var lima = new Store('Lima', 2, 16, 4.6);
+
+
+
+
 
 
 
